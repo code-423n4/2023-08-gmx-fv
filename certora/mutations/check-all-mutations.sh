b7c39c4b2ad782854fea2fe4d151c07e336439c4
teryanarmen <61996358+teryanarmen@users.noreply.github.com>
2023-08-17 09:35:37 -0700

    update check-all-mutations flag to have a default mutation dir

--- certora/mutations/check-all-mutations.sh
+++ certora/mutations/check-all-mutations.sh
@@ -1,6 +1,9 @@
+# FOLDER == "certora" by default, $1 if passed in
+FOLDER=${1:-"certora"}
+
 # take first input from user and use it as the path to the folder that contains the patch files
 # apply all patches in to munged using git apply and run all scripts in ../scripts/ against each patch file
-for f in certora/mutations/$1/*.patch
+for f in certora/mutations/$FOLDER/*.patch
 do
     echo "Applying $f"
     git apply $f
